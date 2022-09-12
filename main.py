f1bab64bfc6dcf4efb53a0f04838200c895ce2aa
SimBa <sektorious@gmail.com>
2022-09-12 10:45:37 +0200

    Bijgewerkt

--- main.py
+++ main.py
@@ -8,16 +8,22 @@ Player2 = "Marco van Basten"
 goal_0 = 32
 goal_1 = 54
 scorers = Player1 + " " + str(goal_0) + ", " + Player2 + " " + str(goal_1)
-report = Player1 + " " + "scored in the " + str(goal_0) +"nd minute\n" + Player2 + " " + "scored in the " + str(goal_1) + "th minute" 
+#report1 = Player1 + " " + "scored in the " + str(goal_0) +"nd minute\n" + Player2 + " " + "scored in the " + str(goal_1) + "th minute" 
+report = f'{Player1} scored in the {str(goal_0)}nd minute' + "\n" f'{Player2} scored in the {str(goal_1)}th minute' 
+"""
 
-player = "Ruud Gullit"
-first_name_start = player.find("Ruud")
-first_name=player[first_name_start:4]
+Use f-strings -or- the +-operator to create a single string with information about who scored when in the format:
+
+Dacht zodoende dat het ook mocht :)
+"""
+
+player = "Marco van Basten"
+first_name_end=player.find(" ") # zolang een dubbele voornaam met een - is.. 
+first_name = player[:first_name_end]
+last_name = player[(first_name_end+1):] 
 first_name_len=len(first_name)
-
-last_name_start = player.find("Gullit")
-last_name=player[last_name_start:11]
-last_name_len=len(last_name)  
+last_name_len=len(last_name)
+print (first_name, last_name)
 
 name_short=first_name[0:1] + ". " + last_name
 
@@ -25,3 +31,5 @@ chant = (first_name+"! ")*(first_name_len-1)+first_name+"!"
 chant_len=len(chant)
 last_char=chant[chant_len-1]
 good_chant=(last_char!=" ")
+
+print (chant)
